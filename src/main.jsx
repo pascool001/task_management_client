8dc63a217b4d592ee43bc7ffb4bca123122fa1c6
pascool001 <pascool404@gmail.com>
2024-07-25 10:41:40 +0000

    favicon problem_3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './utils/darkMode';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
@@ -14,12 +14,12 @@ import { persistor, store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
           <PersistGate  loading={null} persistor={persistor}>
             <App />
           </PersistGate>
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
 )
